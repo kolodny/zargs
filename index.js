6ac54174ce1836d1185a40e460c2749f93300bfa
moshek <mkolodny@integralads.com>
2014-09-18 23:12:06 -0400

    get rid of .8

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 module.exports = zarg;
 
 function zarg(options) {
-    var yargs = require('yargs');
-    (options || []).forEach(function(option) {
-        var key = option.key;
-        yargs = yargs.options(key, {});
+  var yargs = require('yargs');
+  (options || []).forEach(function (option) {
+    var key = option.key;
+    yargs = yargs.options(key, {});
 
-        'alias default demand describe type'.split(' ').forEach(function(method) {
-            if (method in option) {
-                yargs = yargs[method](key, option[method]);
-            }
-        });
+    'alias default demand describe type'.split(' ').forEach(function (method) {
+      if (method in option) {
+        yargs = yargs[method](key, option[method]);
+      }
     });
-    return yargs;
+  });
+  return yargs;
 }
--- test/index.js
+++ test/index.js
@@ -1,35 +1,39 @@
 var assert = require('assert');
 var zargs = require('..');
 
-describe('zargs', function() {
-    it('should work without any args', function() {
-        assert.doesNotThrow(function() {
-            zargs().parse('');
-        });
+describe('zargs', function () {
+  it('should work without any args', function () {
+    assert.doesNotThrow(function () {
+      zargs().parse('');
     });
+  });
 
-    it('should use demand', function() {
-        assert.throws(function() {
-            zargs({key: 'r', demand: true}).parse('');
-        });
+  it('should use demand', function () {
+    assert.throws(function () {
+      zargs({key: 'r', demand: true}).parse('');
     });
+  });
 
-    it('should use default', function() {
-        var args = zargs([{
-            key: 'd',
-            default: 123
-        }]).parse('');
-        assert.equal(args.d, 123);
-    });
+  it('should use default', function () {
+    var args = zargs([
+      {
+        key: 'd',
+        default: 123
+      }
+    ]).parse('');
+    assert.equal(args.d, 123);
+  });
 
-    it('should use describe', function() {
-        var describe = 'this is desc';
-        var args = zargs([{
-            key: 'd',
-            describe: describe
-        }]);
-       assert(args.help().indexOf(describe) > -1);
+  it('should use describe', function () {
+    var describe = 'this is desc';
+    var args = zargs([
+      {
+        key: 'd',
+        describe: describe
+      }
+    ]);
+    assert(args.help().indexOf(describe) > -1);
 
-    });
+  });
 
 });
